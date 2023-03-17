e68a2f4e2a59ae61c03f650287b8411bf642c154
chandrachristian <2110631250035@student.unsika.ac.id>
2023-03-17 12:40:02 +0700

    second

---
+++ src/pbo/pertemuan4/Abstract2/Circle.java
@@ -0,0 +1,14 @@
+package pbo.pertemuan4.Abstract2;
+
+public class Circle extends Shape {
+    private float radius;
+
+    public Circle(float radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    float getArea() {
+        return (float) (Math.PI * radius * radius);
+    }
+}
