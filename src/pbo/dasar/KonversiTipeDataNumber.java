6a148c07e1c9007232b42c053e58e5759b66c795
chandrachristian <2110631250035@student.unsika.ac.id>
2023-04-02 16:37:52 +0700

    nothing

--- src/pbo/dasar/KonversiTipeDataNumber.java
+++ src/pbo/dasar/KonversiTipeDataNumber.java
@@ -1,17 +1,17 @@
 package pbo.dasar;
 
 public class KonversiTipeDataNumber {
-    
+
     /**
      * @param args
      */
     public static void main(String[] args) {
-        
-        byte iniByte = 10;
-        short iniShort = iniByte;
-        int iniInt = iniShort;
 
-        int iniInt2 = 1000;
-        byte iniByte2 = (byte) iniInt2;
+        // byte iniByte = 10;
+        // short iniShort = iniByte;
+        // int iniInt = iniShort;
+
+        // int iniInt2 = 1000;
+        // byte iniByte2 = (byte) iniInt2;
     }
 }
