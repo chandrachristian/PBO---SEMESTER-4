1e62b27cee5afaaf3e2ac996176ee410115fd94c
chandrachristian <2110631250035@student.unsika.ac.id>
2023-04-02 08:14:09 +0700

    update extensions

--- src/pbo/pertemuan4/Example1/Main.java
+++ src/pbo/pertemuan4/Example1/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Person pertama = new Person();
         pertama.setName("Chandra");
-        pertama.age = 20;
+        pertama.setAge(20);
 
         System.out.println("Name " + pertama.getName());
         System.out.println("Age " + pertama.getAge());
---
+++ src/pbo/pertemuan5/tugas/MainKalkulator.java
@@ -0,0 +1,58 @@
+package pbo.pertemuan5.tugas;
+
+import java.util.Scanner;
+
+class Kalkulator {
+    int a;
+    int b;
+
+    void hello(String nama) {
+        System.out.println("Selamat datang " + nama + " di Kalkulator Mandiri\n");
+    }
+
+    void Pertambahan() {
+
+        int hasilPertambahan = a + b;
+        System.out.println("Pertambahan " + a + " + " + b + " = " + hasilPertambahan);
+    }
+
+    void Pengurangan() {
+
+        int hasilPengurangan = a - b;
+        System.out.println("Pengurangan " + a + " - " + b + " = " + hasilPengurangan);
+    }
+
+    int Perkalian(int a, int b) {
+        int hasilPerkalian = a * b;
+        System.out.print("Perkalian " + a + " * " + b + " = ");
+        return hasilPerkalian;
+    }
+
+    int Pembagian() {
+        int hasilPembagian = a / b;
+        System.out.println("Pembagian " + a + " / " + b + " = " + hasilPembagian);
+        return hasilPembagian;
+    }
+}
+
+public class MainKalkulator {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        Kalkulator obj = new Kalkulator();
+
+        System.out.print("Masukkan Angka 1 : ");
+        obj.a = input.nextInt();
+        System.out.print("Masukkan Angka 2 : ");
+        obj.b = input.nextInt();
+
+        obj.hello("Chandra");
+        obj.Pertambahan();
+        obj.Pengurangan();
+        System.out.println(obj.Perkalian(obj.a, obj.b));
+        obj.Pembagian();
+
+        input.close();
+    }
+}
