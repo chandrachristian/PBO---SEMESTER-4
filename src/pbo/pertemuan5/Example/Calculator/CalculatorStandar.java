230edc5a441628aaca123448114dcf92eb28099c
chandrachristian <2110631250035@student.unsika.ac.id>
2023-04-10 16:28:30 +0700

    updating

--- src/pbo/pertemuan5/Example/Calculator/CalculatorStandar.java
+++ src/pbo/pertemuan5/Example/Calculator/CalculatorStandar.java
@@ -27,20 +27,20 @@ public class CalculatorStandar {
 
     void pengurangan() {
         int result = number1 - number2;
-        System.out.println("Hasil Pengurangan " + number1 + " + " + number2 + "\t: " + result);
+        System.out.println("Hasil Pengurangan " + number1 + " - " + number2 + "\t: " + result);
     }
 
     int perkalian(int a, int b) {
         setNumber1(a);
         setNumber2(b);
         int result = a * b;
-        System.out.println("Hasil Perkalian " + number1 + " + " + number2 + "\t\t: " + result);
+        System.out.println("Hasil Perkalian " + number1 + " * " + number2 + "\t\t: " + result);
         return result;
     }
 
     int pembagian() {
         int result = number1 / number2;
-        System.out.println("Hasil Pembagian " + number1 + " + " + number2 + "\t\t: " + result);
+        System.out.println("Hasil Pembagian " + number1 + " / " + number2 + "\t\t: " + result);
         return result;
     }
 }
